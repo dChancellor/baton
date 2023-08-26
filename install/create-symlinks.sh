a332f8c5b8abc3c0f04d7403fce4d7ce82c624e8
Chancellor Holtzman <me@chancellor.tech>
2023-08-26 08:42:03 +0000

    before trying neovim install

--- install/create-symlinks.sh
+++ install/create-symlinks.sh
@@ -1,5 +1,27 @@
-# things to create symlinks for
-# .profile
-# configs folder
-# nvim
-# scripts folder
+create_symlink() {
+    # Check if the right number of arguments are provided
+    if [[ $# -ne 2 ]]; then
+        echo "Usage: create_symlink <source> <link_target>"
+        return 1
+    fi
+
+    local source_target="$1"
+    local link_target="$2"
+
+    # Check if the source target exists
+    if [[ ! -e $source_target ]]; then
+        echo "Error: Source target '$source_target' does not exist."
+        return 1
+    fi
+
+    # Ensure the parent directory of the link target exists
+    mkdir -p "$(dirname "$link_target")"
+
+    # Create the symlink
+    ln -s "$source_target" "$link_target"
+}
+
+create_symlink "$HOME/setup/bash/.profile" "~/.profile"
+create_symlink "$HOME/setup/configs/" "$CONFIG_DIR"
+create_symlink "$HOME/setup/nvim/" "/.config/nvim"
+create_symlink "$HOME/setup/scripts/" "$SCRIPTS_DIR"
---
+++ install/install-dev.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install ninja-build gettext cmake unzip curl
+git clone https://github.com/neovim/neovim
+
