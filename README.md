00b975db2af8607c71b78db616dff86784e14a2b
Chancellor Holtzman <me@chancellor.tech>
2023-08-26 09:26:38 +0000

    adding more submodules

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 1. Install nvim
 2. Install tmux
 3. Install fzf
-4. Install Obsidian
--- install/install-dev.sh
+++ install/install-dev.sh
@@ -1,5 +1,19 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install ninja-build gettext cmake unzip curl
-git clone https://github.com/neovim/neovim
 
+# Install Neovim
+sudo apt-get install ninja-build gettext cmake unzip curl
+make -C "$INSTALL_PATH"/neovim CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make -C "$INSTALL_PATH"/neovim install
+
+# Install Tmux
+sudo apt-get install libevent-dev ncurses-dev build-essential bison pkg-config autotools-dev automake
+sh "$INSTALL_PATH"/tmux/autogen.sh
+$INSTALL_PATH/tmux/.configure
+make -C "$INSTALL_PATH"/tmux
+sudo make -C "$INSTALL_PATH"/tmux install
+
+# Install fzf
+"$INSTALL_PATH"/fzf/install --all
+
+rm -rf "$INSTALL_PATH"/bin
