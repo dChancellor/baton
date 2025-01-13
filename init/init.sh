d6c3f39b55239cf85af73ee9eea12f068dec8a5c
Derek Holtzman <me@chancellor.tech>
2025-01-12 22:22:20 -0600

    this is where im at

--- init/init.sh
+++ init/init.sh
@@ -1,9 +1,9 @@
 GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
 
-source $GIT_ROOT_DIR/scripts/pull_submodules.sh
+source $GIT_ROOT_DIR/scripts/pull_submodules
 
 # Detect the operating system and put it into the OS variable
-source $GIT_ROOT_DIR/scripts/check_os.sh
+source $GIT_ROOT_DIR/scripts/check_os
 
 # Install ansible
 source $GIT_ROOT_DIR/init/install_ansible.sh
