0093a3ee42c7b9ae9154d2dce3257174d4a454c7
Derek Holtzman <me@chancellor.tech>
2025-02-15 00:40:47 -0600

    delete and clarify

--- init/init.sh
+++ init/init.sh
@@ -8,8 +8,5 @@ source $BATON_ROOT_DIR/scripts/check_os
 # Install ansible
 source $BATON_ROOT_DIR/init/install_ansible.sh
 
-# ANSIBLE_NOCOWS=1 ANSIBLE_ROLES_PATH="$BATON_ROOT_DIR/ansible/roles" ansible-playbook -i "$BATON_ROOT_DIR/ansible/inventory.yaml" "$PLAYBOOK_PATH" --ask-become-pass
-
-# ANSIBLE_NOCOWS=1 ANSIBLE_ROLES_PATH="$BATON_ROOT_DIR/ansible/roles" ansible-playbook -i "$BATON_ROOT_DIR/ansible/inventory.yaml", --connection=local "$BATON_ROOT_DIR/ansible/playbooks/development-machines.ansible.yaml" --ask-become-pass
-
+# Runs local ansible playbook to install all the necessary dependencies for development
 source $BATON_ROOT_DIR/ansible/run_playbook "$BATON_ROOT_DIR/ansible/playbooks/development-machines.ansible.yaml"
