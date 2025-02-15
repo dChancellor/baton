7448166f4222f9835e226d0d51cbd6610a47f8a6
Derek Holtzman <me@chancellor.tech>
2025-02-15 00:39:48 -0600

    loads of stuff

--- init/init.sh
+++ init/init.sh
@@ -1,11 +1,15 @@
-GIT_ROOT_DIR="$(git rev-parse --show-toplevel)"
+BATON_ROOT_DIR="$(git rev-parse --show-toplevel)"
 
-source $GIT_ROOT_DIR/scripts/pull_submodules
+source $BATON_ROOT_DIR/scripts/pull_submodules
 
 # Detect the operating system and put it into the OS variable
-source $GIT_ROOT_DIR/scripts/check_os
+source $BATON_ROOT_DIR/scripts/check_os
 
 # Install ansible
-source $GIT_ROOT_DIR/init/install_ansible.sh
+source $BATON_ROOT_DIR/init/install_ansible.sh
 
-ANSIBLE_NOCOWS=1 ansible-playbook -i localhost, --connection=local $GIT_ROOT_DIR/ansible/playbooks/development-machines.ansible.yaml --ask-become-pass
+# ANSIBLE_NOCOWS=1 ANSIBLE_ROLES_PATH="$BATON_ROOT_DIR/ansible/roles" ansible-playbook -i "$BATON_ROOT_DIR/ansible/inventory.yaml" "$PLAYBOOK_PATH" --ask-become-pass
+
+# ANSIBLE_NOCOWS=1 ANSIBLE_ROLES_PATH="$BATON_ROOT_DIR/ansible/roles" ansible-playbook -i "$BATON_ROOT_DIR/ansible/inventory.yaml", --connection=local "$BATON_ROOT_DIR/ansible/playbooks/development-machines.ansible.yaml" --ask-become-pass
+
+source $BATON_ROOT_DIR/ansible/run_playbook "$BATON_ROOT_DIR/ansible/playbooks/development-machines.ansible.yaml"
